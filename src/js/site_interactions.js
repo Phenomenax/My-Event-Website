a344d7093ea84a5f84ef9c20a580c969b9224442
Phenomenax <liyuanzhuo0704@gmail.com>
2023-08-12 21:58:45 +1000

    Button upgraded
    upgraded the discount code button

--- src/js/site_interactions.js
+++ src/js/site_interactions.js
@@ -2,9 +2,11 @@ let discount_code = 5000;
 
 const discount_button = document.getElementById('discount-code');
 
-const showDiscountCode = () => {
+const your_discount_code_element = document.getElementById('your-discount-code');
+
+function showDiscountCode() {
     discount_code = discount_code + 1;
-    alert("Your discount code is " + discount_code);
+    your_discount_code_element.innerText = "CODE is " + discount_code;
 }
 
 discount_button.addEventListener("click", showDiscountCode);
